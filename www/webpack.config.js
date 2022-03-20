9d195c34f8c4f394a44c8b45ad299cf1a04b3879
Adrian Hornung <hornuadr@students.zhaw.ch>
2022-03-20 22:21:17 +0100

    rename

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -1,4 +1,4 @@
-const { CopyWebpackPlugin  } = require("copy-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const path = require('path');
 
